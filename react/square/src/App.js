4ffb6d71cfec34ff9a30f87081c32da24234344d
Nick <neverate@gmail.com>
2019-06-24 18:17:28 +0300

    added an example of using higher order functions to extend the functionality of a component

--- react/square/src/App.js
+++ react/square/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Square from './Square';
+import SimpleSquare from './SimpleSquare';
+import Hideable from './Hideable';
+
+const HideableSquare = Hideable(SimpleSquare);
 
 function App() {
   return (
@@ -9,9 +13,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <Square isHidden={true} size={50}/>
+          <Square isHidden={true} size={60}/>
           <Square initialColor={"blue"} size={80}/>
           <Square initialColor={"green"} size={100}/>
+          <HideableSquare isHidden={true} size={40} />
+          <HideableSquare isHidden={false} size={50} color={'cyan'} />
+          <SimpleSquare size={70}/>
         </div>        
       </header>
     </div>

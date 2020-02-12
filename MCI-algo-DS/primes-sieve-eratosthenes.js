310c047a1c899b4b4eb03b27c59658a3cd30e090
Nick <neverate@gmail.com>
2020-02-12 13:37:50 +0200

    + upd sieve, improved the algo a lil bit

--- MCI-algo-DS/primes-sieve-eratosthenes.js
+++ MCI-algo-DS/primes-sieve-eratosthenes.js
@@ -3,23 +3,31 @@ let n = 10000000;
 const getPrimes = function(n) {
   const notPrimes = [];
   const primes = [];
+  let whileIterations = 0;
 
   for (let i = 2; i <= n; i++) {
     if (!notPrimes[i]) {
       primes.push(i);
-      let j = i;
+      //j = i * i instead of j = i since all multiples of i
+      //from i to i * i will be multiples of previous primes
+      //that we have crossed off already
+      // 25% fewer iterations of while loop for n = 10mil
+      // down from 30m to 23m
+      let j = i * i;
       while (true) {
-        j += i;
+        whileIterations++;
         if (j <= n) {
           notPrimes[j] = true;
         } else {
           break;
         }
+        j += i;
       }
     }
   }
 
   console.log(primes.toString());
+  console.log(`\nNr. of while iterations: ${whileIterations}`);
   return primes.length;
 };
 

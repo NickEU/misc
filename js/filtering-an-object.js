a63acf3b5bea8126f009116ae20060a7fe341566
Nick <neverate@gmail.com>
2020-03-09 09:36:31 +0200

    formatting

--- js/filtering-an-object.js
+++ js/filtering-an-object.js
@@ -1,33 +1,32 @@
 const usersById = {
-  "5": { "id": "5", "name": "Adam", "registered": true },
-  "27": { "id": "27", "name": "Bobby", "registered": true },
-  "32": { "id": "32", "name": "Clarence", "registered": false },
-  "39": { "id": "39", "name": "Danielle", "registered": true },
-  "42": { "id": "42", "name": "Ekaterina", "registered": false },
-}
+  "5": { id: "5", name: "Adam", registered: true },
+  "27": { id: "27", name: "Bobby", registered: true },
+  "32": { id: "32", name: "Clarence", registered: false },
+  "39": { id: "39", name: "Danielle", registered: true },
+  "42": { id: "42", name: "Ekaterina", registered: false }
+};
 
-const getRegisteredUsers = (obj) =>  {
-let filteredObj = {};
-for (let item of Object.entries(obj)) {
-  let [key, value] = item;
-  if (value.registered) {
-    filteredObj[key] = value;
+const getRegisteredUsers = obj => {
+  let filteredObj = {};
+  for (let item of Object.entries(obj)) {
+    let [key, value] = item;
+    if (value.registered) {
+      filteredObj[key] = value;
+    }
   }
-}
-return filteredObj;
-}
+  return filteredObj;
+};
 
-const getRegisteredUsersArr = (obj) => { 
-let keysArr = Object.keys(obj);
-keysArr = keysArr.map( (item) => {
-  return {
-    [item] : obj[item],
-  }// leaves the nesting and 'id: obj' KVP structure intact
-});
-return keysArr.filter((el) => el[Object.keys(el)].registered);
-}
-
+const getRegisteredUsersArr = obj => {
+  let keysArr = Object.keys(obj);
+  keysArr = keysArr.map(item => {
+    return {
+      [item]: obj[item]
+    }; // leaves the nesting and 'id: obj' KVP structure intact
+  });
+  return keysArr.filter(el => el[Object.keys(el)].registered);
+};
 
 console.log(getRegisteredUsers(usersById));
-console.log('');
+console.log("");
 console.log(getRegisteredUsersArr(usersById));

29c05b14bda7f9c29042cd8018ed6970d1638a7f
Nick <neverate@gmail.com>
2020-04-15 12:42:19 +0300

    upd skiena testsuite

--- skiena/1-insertion/test.mjs
+++ skiena/1-insertion/test.mjs
@@ -6,32 +6,39 @@ const testCases = [
   ['654321', '123456'],
 ];
 
-runTestSuite(testCases);
+runTestSuite(testCases, sortArray);
 
-function runTestSuite(testCases) {
+function runTestSuite(testCases, funcBeingTested) {
+  console.log(`\nTesting ${funcBeingTested.name}:`);
   let allTestsPassed = true;
 
   testCases.forEach((test, i) => {
-    if (!runTest(i + 1, test[0], test[1])) {
+    if (!runTest(i + 1, test[0], test[1], funcBeingTested)) {
       allTestsPassed = false;
     }
   });
 
   if (allTestsPassed) {
-    console.log('All tests have passed!');
+    console.log('\x1b[32m', '\nAll tests have passed!');
   }
 }
 
-function runTest(testNumber, input, expectedOutput) {
-  console.log(`Running test #${testNumber}:`);
+function runTest(testNumber, input, expectedOutput, funcBeingTested) {
+  console.log('\x1b[0m', `\nRunning test #${testNumber}:\n`);
   console.log(`Input: ${input}`);
 
-  const actualOutput = sortArray(input);
+  const actualOutput = funcBeingTested(input);
 
-  console.log(`Expected = ${expectedOutput}\nActual = ${actualOutput}`);
-  const isTestSuccesful = actualOutput == expectedOutput;
+  console.log(`Expected = ${expectedOutput}\nActual   = ${actualOutput}`);
 
-  console.log(`Test #${testNumber} has`, isTestSuccesful ? 'passed' : 'failed');
+  const isTestSuccessful = actualOutput == expectedOutput;
+  const textColor = isTestSuccessful ? '\x1b[32m' : '\x1b[31m';
 
-  return isTestSuccesful;
+  console.log(
+    textColor,
+    `Test #${testNumber} has`,
+    isTestSuccessful ? 'passed' : 'failed'
+  );
+
+  return isTestSuccessful;
 }

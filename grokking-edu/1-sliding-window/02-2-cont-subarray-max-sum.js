3bf56617418c6e726eb55a7a4d238195ae5a8cdf
Nick <neverate@gmail.com>
2020-04-26 21:04:47 +0300

    grokking-edu sliding window 2-2 solution

---
+++ grokking-edu/1-sliding-window/02-2-cont-subarray-max-sum.js
@@ -0,0 +1,32 @@
+'use strict';
+// Given an array of positive numbers and a positive number ‘k’,
+// find the maximum sum of any contiguous subarray of size ‘k’.
+
+// Input: [2, 1, 5, 1, 3, 2], k=3
+// Output: 9
+// Explanation: Subarray with maximum sum is [5, 1, 3].
+
+// Input: [2, 3, 4, 1, 5], k=2
+// Output: 7
+// Explanation: Subarray with maximum sum is [3, 4].
+
+
+const findMaxSum = (arr, k) => {
+  let maxSum = -Infinity;
+  let tempSum = 0;
+  let leftIdx = 0;
+  for (let rightIdx = 0; rightIdx < arr.length; rightIdx++) {
+    tempSum += arr[rightIdx];
+    if (rightIdx >= k - 1) {
+      console.log(tempSum);
+      maxSum = Math.max(maxSum, tempSum);
+      tempSum -= arr[leftIdx];
+      leftIdx++;
+    }
+  }
+
+  return maxSum;
+};
+
+console.log(findMaxSum([2, 1, 5, 1, 3, 2], 3) === 9);
+console.log(findMaxSum([2, 3, 4, 1, 5], 2) === 7);
